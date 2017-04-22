b5f9047bf15ae7fad646695dba1660967b45d71a
MeenakshiParyani <meenakshi.paryani@sjsu.edu>
2017-04-22 10:46:53 -0700

    Adding My Bag View

--- portal/StarbucksWebPortal/public/angularjs/ang_order.js
+++ portal/StarbucksWebPortal/public/angularjs/ang_order.js
@@ -4,9 +4,13 @@ angular.module('starbucks').controller('orderCtrl', [ '$rootScope', '$scope', '$
 	$scope.addToCart = function () {
 
 		console.log("Bev type :" + $scope.orderItem.bevName) ;
-		$rootScope.order.bagItems.push($scope.orderItem)
-		resetOrderItem () ;
-		$state.go ( 'home' ) ;
+		var orderItemBag = {
+			bevName : $scope.orderItem.bevName ,
+			qty : $scope.orderItem.qty,milkType : $scope.orderItem.milkType,
+			cupSize : $scope.orderItem.cupSize } ;
+			$rootScope.bagItems.push(orderItemBag);
+			resetOrderItem () ;
+			$state.go ( 'home' ) ;
 
 	}
 
