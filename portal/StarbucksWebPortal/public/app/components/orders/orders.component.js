f87da04640eacec6457c6d92e3d29c528a27f96f
nilamdeka23 <nilampratim.deka@sjsu.edu>
2017-05-12 03:20:59 -0700

    Get Orders Controller

--- portal/StarbucksWebPortal/public/app/components/orders/orders.component.js
+++ portal/StarbucksWebPortal/public/app/components/orders/orders.component.js
@@ -1,16 +1,25 @@
 angular.module('starbucks').component('orders', {
   templateUrl: 'components/orders/orders.component.html',
 
-  controller: ['OrdersService',
-    function OrdersController(OrdersService) {
+  controller: ['$scope','OrdersService',
+    function OrdersController($scope, OrdersService) {
       var self = this;
+      
+      OrdersService.getAllOrders(function(orders) {
+        $scope.orders = orders;
+      });
 
-      this.orders = OrdersService.getAllOrders();
 
+      
       this.removeOrder = function(orderId) {
         OrdersService.removeOrder(orderId).then(function() {
-          self.orders.forEach(function(order, i) {
-            if (order.id === orderId) self.orders.splice(i, 1);
+          console.log("Delelted order ...Now removing from array")
+          
+          $scope.orders.forEach(function(order, i) {
+            if ((order._id || order.id) === orderId){ 
+            $scope.orders.splice(i, 1);
+            console.log($scope.orders);
+          }
           });
         });
       };
